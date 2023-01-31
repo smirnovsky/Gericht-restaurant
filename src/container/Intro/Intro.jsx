98c16b19ac850d3afe2ddc4737767e918af99cb0
smirnovsky <happy280190@gmail.com>
2023-01-31 20:45:53 +0300

    feat: add layout and style for intro video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
 import './Intro.css';
+import { BsPlayFill, BsPauseFill } from 'react-icons/bs'
+import { meal } from '../../constants';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+
+    if(playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play()
+    }
+  }
+
+  return (
+    <div className='app__video'>
+      <video 
+        src={meal} 
+        ref={vidRef}
+        type='video/mp4' 
+        loop 
+        controls={false} 
+        muted 
+      />
+      <div className='app__video-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center'
+        onClick={handleVideo}
+      >
+        {playVideo 
+          ? <BsPauseFill color='#fff' fontSize={30} />
+          : <BsPlayFill color='#fff' fontSize={30} /> }
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Intro;
