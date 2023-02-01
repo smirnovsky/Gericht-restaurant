ef9976e391acb3e20019207fbabf3985241ff6a7
smirnovsky <happy280190@gmail.com>
2023-02-01 16:01:47 +0300

    feat: add style for gallery component

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -41,7 +41,7 @@ const Gallery = () => {
             </div>
           ))}
         </div>
-        <div className='app__gallery-images_arrow'>
+        <div className='app__gallery-images_arrows'>
           <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
           <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
         </div>
